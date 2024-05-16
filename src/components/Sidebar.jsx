5033c6d73cca7913cbbb0e63bde3a3cc00c487ce
Anupam Shakya <69745638+callmeanupam@users.noreply.github.com>
2024-05-16 15:45:57 +0530

    Added Player Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -32,6 +32,13 @@ const Sidebar = () => {
             Create Playlist
           </button>
         </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
+          <h1>Podcasts to follow</h1>
+          <p className="font-light">Check new episodes</p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Browse Podcasts
+          </button>
+        </div>
       </div>
     </div>
   );
